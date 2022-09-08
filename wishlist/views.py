997b2d1af005005701c723133adefb5fc1dcd6a7
kausarme <67036164+kausarme@users.noreply.github.com>
2022-09-08 11:16:58 +0700

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from wishlist.models import BarangWishlist
 
 # Create your views here.
 def show_wishlist(request):
     return render(request, "wishlist.html")
+
